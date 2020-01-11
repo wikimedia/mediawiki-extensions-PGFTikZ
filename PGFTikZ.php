c3b555bf7739f8fcfa05b3b7dbc7f1465b08c11e
Zoranzoki21 <zorandori4444@gmail.com>
2020-01-11 16:00:55 +0100

    Convert PGFTikZ to use extension registration
    Bug: T242512
    Change-Id: I7795f4890eaf7ffd3b3f6350354b280fe96c9adb

--- PGFTikZ.php
+++ PGFTikZ.php
@@ -14,70 +14,16 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-/**
- * Protect against register_globals vulnerabilities.
- * This line must be present before any global variable is referenced.
- */
-if( !defined( 'MEDIAWIKI' ) )
-{
-        echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
-        die( -1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'PGFTikZ' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['PGFTikZ'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the PGFTikZ extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the PGFTikZ extension requires MediaWiki 1.29+' );
 }
-
-// Extension credits that will show up on Special:Version
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'PGFTikZ',
-	'version'        => '0.3.0',
-	'author'         => array( 'Thibault Marin', 'Markus Bürkler' ),
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:PGFTikZ',
-	'descriptionmsg' => 'pgftikz-desc'
-);
-
-/**
- * Extension class
- */
-$wgAutoloadClasses['PGFTikZHooks'] = dirname( __FILE__ ) . '/PGFTikZ.hooks.php';
-$wgAutoloadClasses['PGFTikZParser'] = dirname( __FILE__ ) . '/PGFTikZ.parser.php';
-$wgAutoloadClasses['PGFTikZCompiler'] = dirname( __FILE__ ) . '/PGFTikZ.compiler.php';
-
-/**
- * Register hooks
- */
-$wgHooks['ParserFirstCallInit'][] = 'PGFTikZHooks::onPGFTikZParserInit';
-$wgHooks['ParserAfterTidy'][]     = 'PGFTikZHooks::onPGFTikZAfterTidy';
-
-/**
- * Internationalization
- */
-$wgMessagesDirs['PGFTikZ'] = __DIR__ . '/i18n';
-
-/**
- * Parameters (modify in LocalSettings.php)
- */
-
-// Default resolution for generated images
-$wgPGFTikZDefaultDPI = 300;
-
-// Full path to latex executable (assume in path if empty)
-$wgPGFTikZLaTeXPath = 'latex';
-
-// Extra options to latex executable (e.g. --shell-escape for special TikZ
-// rendering)
-$wgPGFTikZLaTeXOpts = '-no-shell-escape';
-
-// Full path to 'dvips' executable
-$wgPGFTikZdvipsPath = 'dvips';
-
-// Either use epstool+imagemagick or ghostscript to generate image
-$wgPGFTikZuseghostscript = true;
-
-// Full path to 'epstool' executable
-$wgPGFTikZepstoolPath = 'epstool';
-
-// Full path to 'ghostscript' executable
-$wgPGFTikZghostScriptPath = 'gs';
-
-// Use standalone LaTeX package
-$wgPGFTikZLaTeXStandalone = true;
-
